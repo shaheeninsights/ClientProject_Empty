2e437595474f96b85e5ad7d513be3632bce165ff
Shaheen <shaheeninsights@gmail.com>
2025-09-01 20:30:40 +0100

    New changes to code

--- working_code.py
+++ working_code.py
@@ -7,4 +7,4 @@ food['marital_Divorced'] = food['marital_Divorced'].replace({1:5,0:0})
 food['marital_Married'] = food['marital_Married'].replace({1:4,0:0})
 food['marital_Single'] = food['marital_Single'].replace({1:3,0:0})
 food['marital_Together'] = food['marital_Together'].replace({1:2,0:0})
-food['marital_Widow'] = food['marital_Widow'].replace({1:1,0:0})
+# This is just a comment to show added lines.
